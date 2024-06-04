4173a47ab4fc2946e00f91a5bb4b28aac5c2e3d7
Melvina Jacobs <mjacobs1341@gmail.com>
2024-06-04 08:38:04 -0400

    EventModel

---
+++ java/src/main/java/com/techelevator/model/Event.java
@@ -0,0 +1,44 @@
+package com.techelevator.model;
+import java.util.List;
+
+public class Event {
+
+    private int eventId;
+
+    private String eventName;
+
+    private int organizerId;
+
+
+
+    public Event (int eventId, String eventName, int organizerId) {
+        this.eventId = eventId;
+        this.eventName = eventName;
+        this.organizerId = organizerId;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId (int eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName (String eventName) {
+        this.eventName = eventName;
+    }
+
+    public int getOrganizerId() {
+        return organizerId;
+    }
+
+    public void setOrganizerId (int organizerId) {
+        this.organizerId = organizerId;
+    }
+
+}
