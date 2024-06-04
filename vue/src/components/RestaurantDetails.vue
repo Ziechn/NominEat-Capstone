47245737bd3fb22e0db86c4325ca3f6bc36df285
Lines Sultzer <LinesSultzer@gmail.com>
2024-06-04 16:26:11 -0400

    card html

--- vue/src/components/RestaurantDetails.vue
+++ vue/src/components/RestaurantDetails.vue
@@ -1,19 +1,21 @@
 <template>
     <div class="restaurant-details">
-        <h2 class="restaurant-name">{{ curRestaurant.name }}</h2>
-            <p class="restaurant-category">{{ curRestaurant.category }}</p>
-            <p class="restaurant-price">{{ curRestaurant.price }}</p>
-            <p class="restaurant-address1">{{ curRestaurant.address1 }}</p>
-            <p class="restaurant-city">{{ curRestaurant.city }}</p>
-            <p class="restaurant-state">{{ curRestaurant.state }}</p>
-            <p class="restaurant-zipcode">{{ curRestaurant.zipcode }}</p>
+            <h2 class="restaurant-name">{{ restaurant.name }}</h2>
+
+            <p class="restaurant-category">{{ restaurant.category }}</p>
+            <p class="restaurant-categories">{{ restaurant.categories }}</p>v
+            <p class="restaurant-price">{{ restaurant.price }}</p>
+            <p class="restaurant-address1">{{ restaurant.address1 }}</p>
+            <p class="restaurant-city">{{ restaurant.city }}</p>
+            <p class="restaurant-state">{{ restaurant.state }}</p>
+            <p class="restaurant-zipcode">{{ restaurant.zipcode }}</p>
     </div>
 </template>
 
 <script>
     export default {
         props: {
-            curRestaurant: Object
+            restaurant: Object
         }
     }
 </script>
