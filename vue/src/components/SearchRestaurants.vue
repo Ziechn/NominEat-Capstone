08196a2fc4a33f467a0557190f4305870b848704
Seth Chesky <sethchesky@gmail.com>
2024-06-04 18:32:45 -0500

    added searchRestaurants component. hopefully it works as intended

--- vue/src/components/SearchRestaurants.vue
+++ vue/src/components/SearchRestaurants.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="search-restaurants">
         <h2>Search</h2>
-        <input placeholder="Enter ZIP Code" type="text" v-model="zipCode"/>
-        <button @click="searchByZipCode">Search</button>
+        <form class="search-form" @submit.prevent="searchByZipCode">
+            <input placeholder="Enter ZIP Code" type="text" v-model="zipCode"/>
+            <input placeholder="Enter # to Limit Results" type="number" v-model="limit"/>
+            <button type="submit">Search</button>
+        </form>
 
-        <!-- <div v-if="loading" class="loading">Loading...please wait...</div>
-        loading and error stuff here -->
-        <div v-if="restaurants.length" class="restaurant-cards">
-        <RestaurantCard/>
+        <div v-if="loading" class="loading">Loading...please wait...</div>
+        
+        <div v-if="!loading && restaurants.length" class="restaurant-cards">
+            <RestaurantCard v-for="restaurant in restaurants" v-bind:key="restaurant.id" v-bind:restaurant="restaurant"/>
+        </div>
+
+        <div v-if="!loading && !restaurants.length">
+            No results found...
         </div>
     </div>
 </template>
@@ -24,6 +31,7 @@ export default{
     data(){
         return {
             zipCode: '',
+            limit: 10,
             restaurants: [],
             loading: false
         };
@@ -35,18 +43,18 @@ export default{
     },
     methods: {
         async searchByZipCode() {
+            this.loading = true;
             try {
-                const response = await RestaurantService.list(this.zipCode);
+                const response = await RestaurantService.list(this.zipCode, this.limit);
                 this.restaurants = response.data;
+                this.loading = false;
             } catch (error) {
                 console.error('Error fetching restaurants:', error);
-            }
+                this.loading = false;
+            } 
         }
     }
 }
-
-
-
 </script>
 
 <style>
