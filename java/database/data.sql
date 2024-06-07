a01552bc95ee65e8f95e06c1418d6e9b2883f494
Melvina Jacobs <mjacobs1341@gmail.com>
2024-06-07 14:08:03 -0400

    Updated data

--- java/database/data.sql
+++ java/database/data.sql
@@ -20,6 +20,57 @@ VALUES (1, 1),
        (1, 4),
        (2, 5);
 
---INSERT INTO restaurant_event (event_id, restaurant_id, yes_votes, no_votes )
+INSERT INTO restaurant (name, phone, address1, address2, address3, city, state, zipcode, image_url, menu_url, rating, latitude, longitude)
+VALUES ('Five Guys', '123-456-7890', '123 Melly St', '', '', 'Philadelphia', 'PA', '19107', 'http://example.com/image1.jpg', 'http://example.com/menu1.pdf', 4.5, 40.712776, -74.005974),
+       ('Wendys', '234-567-8900', '456 Christ Ave', '', '', 'Brooklyn', 'NY', '11212', 'http://example.com/image2.jpg', 'http://example.com/menu2.pdf', 4.0, 34.052235, -118.243683),
+       ('Shake Shack', '345-678-9801', '55 Lines Ave', '', '', 'Trenton', 'NJ', '08054', 'http://example.com/image3.jpg', 'http://example.com/menu3.pdf', 3.9, 41.762776, -94.675971),
+       ('McDonalds', '456-675-3210', '837 Seth St', '', '', 'New Castle', 'De', '90301', 'http://example.com/image4.jpg', 'http://example.com/menu2.pdf', 2.6, 14.748235, -158.245973);
+
+INSERT INTO restaurant_event (event_id, restaurant_id, yes_votes, no_votes )
+VALUES (1, 1, 5, 3),
+       (2, 2, 7, 1),
+       (3, 3, 8, 4),
+       (4, 4, 6, 2),
+       (5, 1, 2, 3),
+       (6, 2, 4, 6);
+
+INSERT INTO transactions (transaction_name)
+VALUES ('Order 234'),
+       ('Order 346'),
+       ('Order 673'),
+       ('Order 564');
+
+INSERT INTO restaurant_transactions (restaurant_id, transaction_id)
+VALUES (1, 1),
+       (2, 2),
+       (3, 3),
+       (4, 4);
+
+
+INSERT INTO category (category_name)
+VALUES ('Fast Food'),
+       ('Italian'),
+       ('Mexican'),
+       ('Chinese'),
+       ('Soul Food'),
+       ('Indian');
+
+INSERT INTO restaurant_category (restaurant_id, category_id)
+VALUES      (1, 1),
+            (2, 2),
+            (3, 3),
+            (4, 4);
+
+
+INSERT INTO restaurant_hours (restaurant_id, day_id, day_name, start_time, end_time)
+VALUES (1, 1, 'Monday', '11:00:00', '18:00:00'),
+       (2, 2, 'Tuesday', '11:00:00', '14:00:00'),
+       (3, 3, 'Wednesday', '11:00:00', '20:00:00'),
+       (4, 4, 'Thursday', '11:00:00', '18:00:00'),
+       (1, 5, 'Friday', '11:00:00', '17:00:00'),
+       (2, 6, 'Saturday', '11:00:00', '15:00:00');
+
+
+
 
 COMMIT TRANSACTION;
