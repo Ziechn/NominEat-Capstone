54a6190ae010c5fa498ed3e208a750a85ef15af6
Lines Sultzer <LinesSultzer@gmail.com>
2024-06-12 17:06:07 -0400

    another fix to store index.js

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -86,168 +86,82 @@ export function createStore(currentToken, currentUser) {
           state.filteredRestaurants = state.restaurants;
         }
       }
-    },
-    actions: {
-//initial backup data (comment in and out dont delete)
-// const backupData = [
-//   {
-//     id: 1,
-//     name: 'East Village Pizza',
-//    // zipCode: '11222',
-//     imageUrl: 'https://via.placeholder.com/250',
-//     categories: ['Pizza'],
-//     rating: 3.5,
-//     price: '$$',
-//     address1: '1234 Pizza St, New York, NY',
-//     isOpenNow: true,
-//     menuUrl: 'http://www.eastvillagepizza.com/menu',
-//     hours: [
-//       { day: 1, start: '1100', end: '2200'},
-//       { day: 2, start: '1100', end: '2200'},
-//       { day: 3, start: '1100', end: '2200'},
-//       { day: 4, start: '1100', end: '2200'},
-//       { day: 5, start: '1100', end: '2200'},
-//       { day: 6, start: '1100', end: '2200'},
-//       { day: 7, start: '1100', end: '2200'},
-//     ],
-//     phoneNumber: '555-1234',
-//     status: 'Open',
-//   },
-//   {
-//     id: 2,
-//     name: 'Sushi Place',
-//    // zipCode: '11222',
-//     imageUrl: 'https://via.placeholder.com/250',
-//     categories: [ 'Sushi' ],
-//     // category: 'Sushi',
-//     rating: 4,
-//     price: '$$$',
-//     address1: '678 Sushi Place, New York, NY',
-//     isOpenNow: true,
-//     menuUrl: 'http://www.sushiplace.com/menu',
-//     hours: [
-//       { day: 1, start: '1100', end: '2200'},
-//       { day: 2, start: '1100', end: '2200'},
-//       { day: 3, start: '1100', end: '2200'},
-//       { day: 4, start: '1100', end: '2200'},
-//       { day: 5, start: '1100', end: '2200'},
-//       { day: 6, start: '1100', end: '2200'},
-//       { day: 7, start: '1100', end: '2200'},
-//     ],
-//     phoneNumber: '555-1234',
-//     status: 'Open',
-//   },
-//   {
-//     id: 3,
-//     name: 'Burger House',
-//    // zipCode: '11222',
-//     imageUrl: 'https://via.placeholder.com/250',
-//     categories: ['Burgers'],
-//     rating: 5,
-//     price: '$$',
-//     address1: '1234 Burger Blvd, New York, NY',
-//     isOpenNow: true,
-//     menuUrl: 'http://www.burgerhouse.com/menu',
-//     hours: [
-//       { day: 1, start: '1100', end: '2200'},
-//       { day: 2, start: '1100', end: '2200'},
-//       { day: 3, start: '1100', end: '2200'},
-//       { day: 4, start: '1100', end: '2200'},
-//       { day: 5, start: '1100', end: '2200'},
-//       { day: 6, start: '1100', end: '2200'},
-//       { day: 7, start: '1100', end: '2200'},
-//     ],
-//     phoneNumber: '555-1234',
-//     status: 'Open',
-//     // coordinates;
-//   }
+    }, actions: {
+      //initial backup data (comment in and out dont delete)
+      // const backupData = [
+      //   {
+      //     id: 1,
+      //     name: 'East Village Pizza',
+      //    // zipCode: '11222',
+      //     imageUrl: 'https://via.placeholder.com/250',
+      //     categories: ['Pizza'],
+      //     rating: 3.5,
+      //     price: '$$',
+      //     address1: '1234 Pizza St, New York, NY',
+      //     isOpenNow: true,
+      //     menuUrl: 'http://www.eastvillagepizza.com/menu',
+      //     hours: [
+      //       { day: 1, start: '1100', end: '2200'},
+      //       { day: 2, start: '1100', end: '2200'},
+      //       { day: 3, start: '1100', end: '2200'},
+      //       { day: 4, start: '1100', end: '2200'},
+      //       { day: 5, start: '1100', end: '2200'},
+      //       { day: 6, start: '1100', end: '2200'},
+      //       { day: 7, start: '1100', end: '2200'},
+      //     ],
+      //     phoneNumber: '555-1234',
+      //     status: 'Open',
+      //   },
+      //   {
+      //     id: 2,
+      //     name: 'Sushi Place',
+      //    // zipCode: '11222',
+      //     imageUrl: 'https://via.placeholder.com/250',
+      //     categories: [ 'Sushi' ],
+      //     // category: 'Sushi',
+      //     rating: 4,
+      //     price: '$$$',
+      //     address1: '678 Sushi Place, New York, NY',
+      //     isOpenNow: true,
+      //     menuUrl: 'http://www.sushiplace.com/menu',
+      //     hours: [
+      //       { day: 1, start: '1100', end: '2200'},
+      //       { day: 2, start: '1100', end: '2200'},
+      //       { day: 3, start: '1100', end: '2200'},
+      //       { day: 4, start: '1100', end: '2200'},
+      //       { day: 5, start: '1100', end: '2200'},
+      //       { day: 6, start: '1100', end: '2200'},
+      //       { day: 7, start: '1100', end: '2200'},
+      //     ],
+      //     phoneNumber: '555-1234',
+      //     status: 'Open',
+      //   },
+      //   {
+      //     id: 3,
+      //     name: 'Burger House',
+      //    // zipCode: '11222',
+      //     imageUrl: 'https://via.placeholder.com/250',
+      //     categories: ['Burgers'],
+      //     rating: 5,
+      //     price: '$$',
+      //     address1: '1234 Burger Blvd, New York, NY',
+      //     isOpenNow: true,
+      //     menuUrl: 'http://www.burgerhouse.com/menu',
+      //     hours: [
+      //       { day: 1, start: '1100', end: '2200'},
+      //       { day: 2, start: '1100', end: '2200'},
+      //       { day: 3, start: '1100', end: '2200'},
+      //       { day: 4, start: '1100', end: '2200'},
+      //       { day: 5, start: '1100', end: '2200'},
+      //       { day: 6, start: '1100', end: '2200'},
+      //       { day: 7, start: '1100', end: '2200'},
+      //     ],
+      //     phoneNumber: '555-1234',
+      //     status: 'Open',
+      //     // coordinates;
+      //   }
 
-// ];
-
-const store = _createStore({
-  state: {
-    restaurants: [],
-    events: [],
-    //votes: {},
-    token: localStorage.getItem('token') || '',
-    user: JSON.parse(localStorage.getItem('user')) || {}
-  },
- 
-  mutations: {
-
- 
-    SET_AUTH_TOKEN(state, token) {
-      state.token = token;
-      localStorage.setItem('token', token);
-      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    },
-    SET_USER(state, user) {
-      state.user = user;
-      localStorage.setItem('user', JSON.stringify(user));
-    },
-    SET_EVENTS(state, events) {
-      state.events = events;
-    },
-    SET_RESTAURANTS(state, restaurants) {
-      state.restaurants = restaurants;
-     // state.filteredRestaurants = restaurants;
-    },
-    //       SET_ZIP_CODE(state, zipCode) {
-    //   state.zipCode = zipCode;
-    // },
-    // SET_LIMIT(state, limit) {
-    //   state.limit = limit;
-    // },
-      ADD_RESTAURANT(state, restaurant) {
-      state.restaurants.push(restaurant);
-    },
-  //   SET_VOTES(state, { restaurantId, votes }) {
-  //    state.votes = ??
-  //   }
-  // },
-    LOGOUT(state) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      state.token = '';
-      state.user = {};
-      axios.defaults.headers.common = ['Authorization'];
-    axios.defaults.headers.common = {};
-    },
-  },
-
-    // REMOVE_SELECTED_RESTAURANTS(state, restaurantId) {
-    //   state.selectedRestaurants = state.selectedRestaurants.filter(
-    //     restaurant => restaurant.id !== restaurantId
-    //   );
-    // // },
-    // CREATE_EVENT(state, event) {
-    //   state.events.push(event);
-    // },
-        // ADD_EVENT(state, event) {
-    //   state.events.push(event);
-    // },
-    // SET_ERROR(state, error) {
-    //   state.error = error;
-    // },
-    // SET_LOADING(state, loading) {
-    //   state.loading = loading;
-    // },
-
-    // SET_CATEGORY(state, category) {
-    //   state.category = category;
-    // },
-    // FILTER_BY_CATEGORY(state, category) {
-    //  if (category) {
-    //     state.filteredRestaurants = state.restaurants.filter(restaurant =>
-    //       restaurant.categories.some(cat =>
-    //         cat.title.toLowerCase().includes(category.toLowerCase())
-    //         )
-    //       )   } else { 
-    //   state.filteredRestaurants = state.restaurants;
-    //   }
-    // }
-
-  actions: {
+      // ];
 
 
       async fetchRestaurants({ commit }, { zipCode, limit = 10 }) {
@@ -330,7 +244,7 @@ const store = _createStore({
       }
     },
 
-    plugins: [createPersistedState(),]
+    plugins: [createPersistedState()]
   });
   return store;
 }
