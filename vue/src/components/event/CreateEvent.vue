f95031a3d0dce304877ed160177ff982062de664
Chris Cook <zedziechn@gmail.com>
2024-06-13 13:26:27 -0400

    Create Event works as intended.

--- vue/src/components/event/CreateEvent.vue
+++ vue/src/components/event/CreateEvent.vue
@@ -2,103 +2,70 @@
   <div>
     <HeaderComp />
     <div class="create-event" >
-    <!-- <h3>Create your NominEat event</h3>
-    <eventCreationPopup @eventCreated="handl
-        <h2>Create your NominEat event</h2>   
-        eEventCreated" /> -->
         <h2>Create your NominEAT event</h2>
-    <form @submit.prevent="createEvent"> 
-        <input v-model="eventName" placeholder="Event Name" required />
-        <input v-model="decisionDate"  id="decisionDate" placeholder="decisionDate" required />
-        <input v-model="location" placeholder="location" required />
-        <button type="submit">Submit</button>
-
-        <!-- <input type="datetime-local" name="decisionDate" id="decisionDate" v-model="event.date"> -->
-        <!-- <label for="eventName">Event Name: </label>
-        <input type="text" name="eventName" id="eventName" v-model="event.eventName">
-        <label for="decisionDate">Decision Date: </label>
-        <input type="datetime-local" name="decisionDate" id="decisionDate" v-model="event.date">
-        <button type="submit">Submit</button> -->
-    </form>
+        <form v-on:submit.prevent="createEvent"> 
+            <input v-model="eventName" placeholder="Event Name" required /> {{ eventName }}
+            <input type="datetime-local" v-model="decisionDate"  id="decisionDate" required /> {{ decisionDate }}
+            <button type="submit">Submit</button>
+        </form>
   </div>
-
-<div v-if="eventCreated">
-    <h3>Restaurants for Event: {{ eventName }}</h3>
-    <RestaurantCardForOrganizer v-for="restaurant in restaurants" :key="restaurant.id" :restaurant="restaurant" :eventId="eventId" />
-    <!-- <RestaurantCard v-for="restaurant in restaurants" :key="restaurant.id" :restaurant="restaurant" /> -->
-</div>
 </div>
 </template>
+
 <!-- create event should pull in info from the database and save it - main view for creating events, pulling initial r data from API and saving to database... -->
 <script>
 import HeaderComp from '@/components/HeaderComp.vue';
-//import RestaurantService from '@/services/RestaurantService.js';
-import RestaurantCardForOrganizer from '../restaurant/RestaurantCardForOrganizer.vue';
-import { mapActions, mapState } from 'vuex';
-//import EventService from '@/services/EventService';
+import EventService from '@/services/EventService';
 
 export default {
-    components: { HeaderComp, RestaurantCardForOrganizer },
+    components: { HeaderComp},
     data() {
         return {
                 eventName: '',
-                location: '',
                 decisionDate: '',
-                eventCreated: false,
-                eventId: null
             };
     },
-    // props: ['location', 'restaurants'],
-
-    computed: {
-        ...mapState(['restaurants'])
-    },
+    props: ['restaurants', 'location'],
     methods: {
-        ...mapActions(['createEvent', 'associateRestaurantsWithEvent']),
         createEvent() {
-                        // Create a new event...
-                        //let newEvent = {
-              this.createEvent( {
-                name: this.eventName,
-                location: this.location,
-                decisionDate: this.decisionDate
-            }).then(response => {
-                this.eventId = response.id;
-                this.eventCreated = true;
-                this.associateRestaurantsWithEvent({ eventId: this.eventId, restaurants: this.restaurants.slice(0, 10) });
-            });
+            // Create a new event...
+            let newEvent = {
+                eventName: this.eventName,
+                decisionDate: this.decisionDate,
+                location: this.$store.state.zipCode
+            }
 
-            // console.log(newEvent);
-            // console.log(this.restaurants);
+            console.log(newEvent);
+            console.log(this.restaurants);
 
-            // // Send the event to the API
-            // let eventId = 0;
+            let eventId = 0;
             
-            // RestaurantService.create(newEvent).then(
-            //     (response) => {
-            //         if (response.status === 201) {
-            //             eventId = response.data.eventId;
-            //         }
+            EventService.createEvent(newEvent).then(
+                (response) => {
+                    if (response.status === 201) {
+                        console.log(response.data);
+                        eventId = response.data.eventId;
+                    }
 
-            //     EventService.addRestaurantsToEvent(eventId, this.restaurants).then(
-            //         (response) => {
-            //             if (response.status === 201) {
-            //                 console.log("Restaurants added to the event.");
-            //                 console.log(response.data);
-            //             }
-            //         }
-            //     ).catch(
-            //         (error) => {
-            //             // this.handleErrorResponse(error, "Error associating restaurants with the event.");
-            //             console.log("Error associating restaurants with the event.")
-            //         }
-            //     );
-            // }).catch(
-            //     (error) => {
-            //         this.handleErrorResponse(error, "Error creating an event.");
-            //         console.log("Error creating an event.")
-            //     }
-           // );
+                EventService.addRestaurantsToEvent(eventId, this.restaurants).then(
+                    (response) => {
+                        console.log(response.status);
+                        if (response.status === 201) {
+                            console.log("Restaurants added to the event.");
+                            console.log(response.data);
+                            this.$router.push({name: 'profile'});
+                        }
+                    }
+                ).catch(
+                    (error) => {
+                        console.log("Error associating restaurants with the event.")
+                    }
+                );
+            }).catch(
+                (error) => {
+                    console.log("Error creating an event.")
+                }
+           );
         }
     }
 };
--- vue/src/components/restaurant/SearchRestaurantList.vue
+++ vue/src/components/restaurant/SearchRestaurantList.vue
@@ -2,23 +2,24 @@
 <!-- fetches restaurant data from API displays it and allows saving of ten to database -->
 
 <template> 
+<HeaderComp />
 <div>
-    <HeaderComp />
     <div class="search-restaurant-list">
-    <h2>Search Restaurants</h2>
+        <h2>Search Restaurants</h2>
         <input v-model="zipcode" placeholder="Enter Zipcode" />
         <input v-model="limit" type="number" placeholder="Limit" />
         <input v-model="term" placeholder="Type of Establishment" />
         <button @click="fetchRestaurants">Search</button>
-        <button @click="doIt">Fetch</button>
+
+        <CreateEvent v-bind:restaurants="restaurants" />
 
         <div v-if="restaurants.length">
-    <div v-for="restaurant in restaurants" :key="restaurant.id" >
-    <RestaurantCard :restaurant="restaurant" />
-</div>
-    <button @click="saveRestaurants">Save Restaurants</button>
+            <div v-for="restaurant in restaurants" :key="restaurant.id" >
+                <RestaurantCard :restaurant="restaurant" />
+            </div>
+        <button @click="saveRestaurants">Save Restaurants</button>
+        </div>
     </div>
-</div>
 </div>
 </template>
 
@@ -29,12 +30,14 @@
 import HeaderComp from '@/components/HeaderComp.vue';
 import RestaurantCard from '@/components/restaurant/RestaurantCard.vue';
 import { mapActions, mapState } from 'vuex';
+import CreateEvent from '../event/CreateEvent.vue';
 
 
 export default {
     components: {
         HeaderComp,
-        RestaurantCard
+        RestaurantCard,
+        CreateEvent
     },
     data() {
         return {
